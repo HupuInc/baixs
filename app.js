9be91c41c1bb36580d6fc6119873656f62427d10
dulei <dulei@hupu.com>
2015-08-14 11:26:51 +0800

    Add websocket support

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 var SwaggerExpress = require('swagger-express-mw');
+var WebSocketServer = require('websocket').server;
+
 var app = require('express')();
-module.exports = app; // for testing
-var Cron = require('./cron')
+var Cron = require('./cron');
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+function startWebsocket(httpServer) {
+  var wsServer = new WebSocketServer({
+      httpServer: httpServer,
+      // You should not use autoAcceptConnections for production
+      // applications, as it defeats all standard cross-origin protection
+      // facilities built into the protocol and the browser.  You should
+      // *always* verify the connection's origin and decide whether or not
+      // to accept it.
+      autoAcceptConnections: false
+  });
+
+  wsServer.on('request', function(request) {
+      if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+      }
+
+      var connection = request.accept('baixs-protocol', request.origin);
+      console.log((new Date()) + ' Connection accepted.');
+      connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+      });
+  });
+
+  return wsServer;
+}
 
 var openDb = require('./db');
 var db = openDb(function() {
-
-  var cron = new Cron(db);
-  cron.start();
 
   var config = {
     appRoot: __dirname // required config
   };
 
   app.set('models', db.models);
-
+  console.log('Starting in env', process.env.NODE_ENV);
   SwaggerExpress.create(config, function(err, swaggerExpress) {
     if (err) { throw err; }
 
@@ -22,9 +54,16 @@ var db = openDb(function() {
     swaggerExpress.register(app);
 
     var port = process.env.PORT || 10010;
-    app.listen(port);
+    var httpServer = app.listen(port);
+
+    var wsSocket = startWebsocket(httpServer);
+
+    var cron = new Cron(db, wsSocket);
+    cron.start();
 
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   });
 
 });
+
+module.exports = app; // for testing
--- cron.js
+++ cron.js
@@ -4,13 +4,16 @@ var EventEmitter = require('events').EventEmitter;
 var CHECK_INTERVAL = process.env.CHECK_INTERVAL || 60 * 1000;
 var emitter = new EventEmitter();
 
-function Cron(db) {
+function Cron(db, channel) {
   this.models = db.models;
   this.db = db.instance;
+  // channel is an instance of websocket connection
+  this.channel = channel;
 }
 
 Cron.prototype.start = function() {
-  var models = this.models
+  var models = this.models;
+  var self = this;
 
   function dispatch() {
     // fetch all the Links
@@ -26,13 +29,19 @@ Cron.prototype.start = function() {
   this.timer = setInterval(dispatch, CHECK_INTERVAL);
   emitter.on('data', function execute(data) {
     var task = new models.Task(data.value);
+    console.log('Run a task:', task);
     task.run(function(err) {
       if (err) {
         console.trace(err);
       }
+      else {
+        self.channel.broadcast({
+          messageId: 'refresh',
+          link: task.link
+        });
+      }
     });
   });
-
 };
 
 Cron.prototype.stop = function() {

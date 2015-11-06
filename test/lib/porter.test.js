9c44021f424793c43938010c3a2b12b6b576b6ba
dulei <dulei@hupu.com>
2015-11-06 18:21:10 +0800

    Porter will fetch host list from etcd every 30 seconds

--- test/lib/porter.test.js
+++ test/lib/porter.test.js
@@ -1,13 +1,12 @@
 var nock = require('nock');
 var should = require('should');
+var sinon = require('sinon');
 var _ = require('lodash');
 
 var bootstrap = require('../');
 var models = bootstrap.models;
 var Host = models.Host;
-
 var Porter = require('../../lib/porter');
-var porter = new Porter();
 
 var fixture = require('../fixture');
 var hostOne = fixture.hostOne;
@@ -29,27 +28,28 @@ var mockedResponse = {
 };
 
 describe('Porter', function() {
-  before(function() {
-    nock.disableNetConnect();
-    this.scope = nock('http://localhost:4001/')
-      .get('/v2/keys/hostvars/')
-      .query({ recursive: true })
-      .reply(200, mockedResponse);
-  });
-
-  after(function() {
-    this.scope.done();
-  });
 
   describe('When hosts are fetched from etcd', function() {
-    it('will tranform response into a list of Host', function(done) {
-      porter.fetchFromSource(function(err, hosts) {
-        should.not.exist(err);
+    before(function() {
+      nock.disableNetConnect();
+      this.scope = nock('http://localhost:4001/')
+        .get('/v2/keys/hostvars/')
+        .query({ recursive: true })
+        .twice()
+        .reply(200, mockedResponse);
+    });
 
+    after(function() {
+      this.scope.done();
+    });
+
+    it('will tranform response into a list of Host', function(done) {
+      var porter = new Porter();
+      porter.fetchFromSource(function(err, hosts) {
         hosts.should.be.an.Array;
         hosts.should.have.lengthOf(mockedNodes.length);
         hosts.forEach(function(host) {
-          host.should.be.an.instanceof(Host);
+          host.should.be.an.instanceOf(Host);
           host.should.have.properties('id', 'doc');
         });
 
@@ -63,11 +63,55 @@ describe('Porter', function() {
           return host.ip === '192.168.1.1';
         }));
 
-        done();
+        done(err);
       });
     });
 
-    it('will fetch from etcd automatically every 30 seconds');
+    describe('Fetch from source every 30 seconds', function() {
+      var porter = new Porter();
+
+      before(function() {
+        this.clock = sinon.useFakeTimers();
+      });
+
+      after(function() {
+        this.clock.restore();
+      });
+
+      before(function() {
+        this.stub = sinon.stub(porter, 'fetchFromSource');
+        this.stub.yields(null, []);
+      });
+
+      after(function() {
+        this.stub.restore();
+      });
+
+      it('will save the results into db', function(done) {
+        var theStub = this.stub;
+        var clock = this.clock;
+
+        porter.on('error', done);
+
+        porter.once('change', function() {
+          theStub.calledOnce.should.be.true;
+          theStub.restore();
+
+          porter.once('change', function() {
+            // hostOne: 192.168.1.1
+            Host.fetch('host:192.168.1.1', function(err, host) {
+              host.should.be.an.instanceOf(Host);
+              done(err);
+            });
+          });
+
+          clock.tick(Porter.INTERVAL);
+        });
+
+        porter.start();
+        clock.tick(Porter.INTERVAL);
+      });
+    });
   });
 
   describe('When send a invalidate signal', function() {

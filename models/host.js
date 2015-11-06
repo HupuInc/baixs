433e1d411b67e4ccaa86242effec97a241c06d58
dulei <dulei@hupu.com>
2015-11-06 15:22:28 +0800

    Add a new model class Host

---
+++ models/host.js
@@ -0,0 +1,38 @@
+var util = require('util');
+
+// NS is used as the key of each host
+// e.g.
+//     host 192.168.1.1 has a host name kq-web-1-1-prd.jh
+//     'host:192.168.1.1' will be the key for all the info stored in leveldb
+//     'host:kq-web-1-1-prd.jh' will return 192.168.1.1
+var NS = 'host:%s';
+
+function mountWithProperties(obj) {
+  var properties = ['hostname', 'ip', 'has_problems'];
+
+  properties.forEach(function(prop) {
+    Object.defineProperty(obj, prop, {
+      get: function() {
+        return obj.doc[prop];
+      },
+      enumerable: true,
+    });
+  });
+}
+
+// A host contains attributes
+//   - required: id, doc.hostname, doc.ip, doc.has_problems(default to no)
+//   - optional: doc.mac, doc.parent, doc.domains
+//   doc.domains is an array of domain, which contains attributes
+//     - name, uuid, ip, mac
+function Host(doc) {
+  this.doc = doc;
+  this.id = util.format(NS, doc.ip);
+  mountWithProperties(this);
+}
+
+Host.prototype.save = function(done) {
+  Host.leveldb.put(this.id, this.doc, { valueEncoding: 'json' }, done);
+};
+
+module.exports = Host;
---
+++ test/models/host.test.js
@@ -0,0 +1,40 @@
+require('should');
+var bootstrap = require('../');
+var models = bootstrap.models;
+var Host = models.Host;
+
+var doc = {
+  ip: '192.168.7.6',
+  hostname: 'sample-7-6-prd',
+  has_problems: 'no',
+};
+
+var host = new Host(doc);
+
+describe('Model - Link', function() {
+  describe('Attributes', function() {
+    it('should have some properties', function() {
+      host.should.have.property('doc', doc);
+      host.should.have.property('ip', doc.ip);
+      host.should.have.property('hostname', doc.hostname);
+      host.should.have.property('has_problems', doc.has_problems);
+    });
+  });
+
+  describe('When store a host into db', function() {
+    before(function(done) {
+      host.save(done);
+    });
+
+    after(function(done) {
+      Host.leveldb.del(host.id, done);
+    });
+
+    it('can be read from db again', function(done) {
+      Host.leveldb.get(host.id, function(err, obj) {
+        obj.should.eql(doc);
+        done(err);
+      });
+    });
+  });
+});

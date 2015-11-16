5bfadf7f87f91812936905f594b5777811dca9ee
daijun <daijun@hupu.com>
2015-11-16 22:11:46 +0800

    Add url monitor card into dashboard

--- ui/dashboard.jsx
+++ ui/dashboard.jsx
@@ -2,6 +2,7 @@ var $ = require('jquery');
 var React = require('react');
 var Highcharts = require('react-highcharts/dist/bundle/highcharts');
 var EventStore = require('./store/event');
+var LinkStore = require('./store/link');
 
 var EventItem = new React.createClass({
   render: function() {
@@ -38,6 +39,9 @@ var EventArea = new React.createClass({
   handleChange: function(data) {
     this.setState({data: data});
   },
+  handleClick: function() {
+    $('#benchTab > span').trigger('click');
+  },
   render: function() {
     var events = this.state.data;
     var eventStat = {
@@ -72,7 +76,7 @@ var EventArea = new React.createClass({
     return (
       <div className="row" >
         {eventItems}
-        <a href="javascript:void(0)" className="small-box-footer">更多信息 <i className="fa fa-arrow-circle-right"></i></a>
+        <a href="javascript:void(0)" className="small-box-footer" onClick={this.handleClick}>更多信息 <i className="fa fa-arrow-circle-right"></i></a>
       </div>
     );
   }
@@ -133,10 +137,8 @@ var MttrArea = new React.createClass({
       series: [{name: 'mttr', colorByPoint: true, data: series}]
     };
     return (
-      <div className="div-col col-lg-6">
-        <div className="div-data-table div-content">
-          <Highcharts config={mttrConfig} ref="chart" />
-        </div>
+      <div className="div-data-table div-content">
+        <Highcharts config={mttrConfig} ref="chart" />
       </div>
     );
   }
@@ -197,12 +199,87 @@ var VmmArea = new React.createClass({
           <div className="small-box bg-green">
             <div className="inner">
               <h3>{current}/{total}</h3>
-              <p>统计</p>
+              <p>虚拟机统计</p>
             </div>
           </div>
         </div>
         <div className="div-data-table div-content">
           <Highcharts config={vmmConfig} ref="chart" />
+        </div>
+      </div>
+    );
+  }
+});
+
+var UrlItem = new React.createClass({
+  render: function() {
+    var link = this.props.data.value;
+    var statusClass = '';
+
+    if (link.status == null) {
+      statusClass = 'warning';
+    }
+    else if (link.status >= 300) {
+      statusClass = 'danger';
+    }
+    return (
+      <tr className={statusClass}>
+        <td>{link.url}</td>
+        <td>{link.proxy}</td>
+        <td>{link.status}</td>
+      </tr>
+    );
+  }
+});
+
+var UrlArea = new React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    LinkStore.on('change', this.handleChange);
+    LinkStore.emit('change', LinkStore.toArray());
+  },
+  componentWillUnmount: function() {
+    LinkStore.removeListener('change', this.handleChange);
+  },
+  handleChange: function(data) {
+    this.setState({data: data});
+  },
+  handleClick: function() {
+    $('#urlTab > span').trigger('click');
+  },
+  render: function() {
+    var items = this.state.data;
+    var links = items.map(function(link) {
+      if (link.value.status >= 300) {
+        return (
+          <UrlItem data={link} />
+        );
+      }
+    });
+
+    return (
+      <div className="div-data-table div-content">
+        <div className="div-url-title">
+          <span>URL监控</span>
+        </div>
+        <div className="div-url-content table-responsive">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>URL</th>
+                <th>Proxy</th>
+                <th>状态码</th>
+              </tr>
+            </thead>
+            <tbody>
+              {links}
+            </tbody>
+          </table>
+        </div>
+        <div className="div-url-action">
+          <a href="javascript:void(0)" onClick={this.handleClick}>所有URL监控</a>
         </div>
       </div>
     );
@@ -216,7 +293,10 @@ var Dashboard = new React.createClass({
         <EventArea />
         <div className="row">
           <VmmArea />
-          <MttrArea />
+          <div className="div-col col-lg-6">
+            <UrlArea />
+            <MttrArea />
+          </div>
         </div>
       </div>
     );

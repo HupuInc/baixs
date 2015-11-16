c4b417ca6adbcf1b7296115f2c973df6d97d34bc
daijun <daijun@hupu.com>
2015-11-16 16:43:56 +0800

    Create new class of Postoffice

---
+++ ui/postoffice.js
@@ -0,0 +1,31 @@
+var LinkStore = require('./store/link');
+var EventStore = require('./store/event');
+
+function Postoffice() {
+  this.addressBook = {
+    'link': LinkStore,
+    'event': EventStore,
+  };
+}
+
+Postoffice.prototype.collect = function(evt) {
+  var data = JSON.parse(evt.data);
+  var address = data.id.slice(0, data.id.indexOf('-'));
+  this.sorter(address, data);
+};
+
+Postoffice.prototype.sorter = function(address, data) {
+  var target = this.addressBook[address];
+  if (target) {
+    this.ship(target, data);
+  }
+  else {
+    console.log('Oops, the address of ' + address + ' does not exists.');
+  }
+};
+
+Postoffice.prototype.ship = function(target, data) {
+  target.receive(data);
+};
+
+module.exports = new Postoffice();

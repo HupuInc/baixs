02b818e60443c8ccea81c66b08ddaeb08e2e1ea0
daijun <daijun@hupu.com>
2016-06-03 17:13:00 +0800

    Refactor, redesign vmm tab   - display vmm host cpu cores and total memory   - display vmm host max metric of cpu load and interface traffic during a day

--- ui/vm.jsx
+++ ui/vm.jsx
@@ -59,10 +59,10 @@ var HostList = React.createClass({
           return p + c.domain.length;
         }, 0);
         $('#SpanSearch').html('搜索结果：命中' + count + '条');
-        $('.div-guests-content').hide();
+        $('.div-vm-hosts-list').hide();
         $('.div-radio').hide();
         $('#SearchResult').show();
-        $('.div-vm-host-d').trigger('click');
+        $('.div-vm-hosts-top').trigger('click');
         $('#divSearchIcon').trigger('click');
       }.bind(this),
       error: function(error, status) {
@@ -75,7 +75,7 @@ var HostList = React.createClass({
     this.request(loc);
   },
   handleBack: function() {
-    $('.div-guests-content').hide();
+    $('.div-vm-hosts-list').hide();
     $('.div-radio').show();
     $('#SearchResult').hide();
     var loc = $('.div-radio input:radio:checked').val();
@@ -97,7 +97,7 @@ var HostList = React.createClass({
     });
     return (
       <div>
-        <LocationRadio onRadioEvent={this.handleRadioEvent} />
+
         <div id="SearchResult" className="search-result">
           <span id="SpanSearch"></span>
           &nbsp;&nbsp;&nbsp;&nbsp;
@@ -114,7 +114,7 @@ var HostList = React.createClass({
 var HostItem = React.createClass({
   handleToggle: function(ev) {
     var container = $(this.getDOMNode());
-    container.find('.div-guests-content').slideToggle(300, function() {
+    container.find('.div-vm-hosts-list').slideToggle(300, function() {
       var i = container.find('i');
       if($(i).hasClass('fa-caret-down')) {
         $(i).removeClass('fa-caret-down').addClass('fa-caret-up');
@@ -136,37 +136,66 @@ var HostItem = React.createClass({
       length = item.domain.length;
     }
 
+    // system.cpu.load[percpu,avg1]
+    // net.if.in[eth0]
+    // net.if.out[eth0]
+    // vfs.dev.read[sdb, ops,]
+    // vfs.dev.write[sdb, ops,]
+    // vm.memory.size[total]
+    // system.cpu.num
+
+    var cpuLoad = item.metrics['system.cpu.load[percpu,avg1]'];
+    var cpuProgressLength = cpuLoad * 100;
+    var trafficIn = item.metrics['net.if.in[eth0]'] / 1000 / 1000;
+    var trafficOut = item.metrics['net.if.out[eth0]'] / 1000 / 1000;
+    var trafficProgressLength = trafficIn / (trafficIn + trafficOut) * 100
+
     return (
-      <div className="div-vm-host-item">
-      <div className="div-vm-host-d row" onClick={this.handleToggle}>
-      <span className="span-vm-icon col-md-1 col-xs-1">
-        <i className="fa fa-circle"> </i>
-      </span>
-      <div className="col-md-10 col-xs-10">
-      <span className="span-vm-hostname">{item.hostname}</span>
-      <span>----</span>
-      <span className="span-vm-ip">{item.ip}</span>
-      <span className="span-badge badge">{length}</span>
-      </div>
-      <span className="span-collapse-expand"><i className="fa fa-caret-down"> </i></span>
-      </div>
-      <div className="div-guests-content">
-        <div className="tb-guest-item table-responsive">
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>状态</th>
-              <th>主机名</th>
-              <th>IP地址</th>
-              <th>KVM域</th>
-            </tr>
-          </thead>
-          <tbody>
-            {vmList}
-          </tbody>
-        </table>
+      <div className="div-vm-host-out col-md-6 col-xs-12">
+        <div className="div-vm-host-item">
+          <div className="div-vmm-title">
+            <div className="div-vmm-title-left">
+              <span className="span-vmm-hostname">{item.hostname}</span>
+              <span className="span-vmm-ip">IP: {item.ip}</span>
+            </div>
+            <div className="div-vmm-title-center">
+              <span className="span-badge badge">{length}</span>
+            </div>
+            <div className="div-vmm-title-right">
+              <span className="span-vmm-c-m">{item.metrics['system.cpu.num']} C / {(item.metrics['vm.memory.size[total]'] / 1024 / 1024 / 1024).toFixed(2)} G</span>
+            </div>
+          </div>
+          <div className="div-vmm-status">
+              <div className="progress-group">
+                <span className="progress-text">CPU Load</span>
+                <span className="progress-number">{cpuLoad} / 1</span>
+                <div className="progress sm">
+                  <div className="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style={{width: cpuProgressLength + '%'}}>
+                  </div>
+                </div>
+              </div>
+              <div className="progress-group">
+                <span className="progress-text">eth0 Traffic</span>
+                <span className="progress-number">IN: {trafficIn.toFixed(2)} Mb / OUT: {trafficOut.toFixed(2)} Mb</span>
+                <div className="progress sm">
+                  <div className="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style={{width: trafficProgressLength + '%'}}>
+                  </div>
+                  <div className="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style={{width: 100 - trafficProgressLength + '%'}}>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <div className="div-vm-hosts">
+            <div className="div-vm-hosts-top" onClick={this.handleToggle}>
+              <span className="span-collapse-expand">
+                <i className="fa fa-caret-down"> </i>
+              </span>
+            </div>
+            <div className="div-vm-hosts-list">
+              {vmList}
+            </div>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
@@ -176,12 +205,14 @@ var VmList = React.createClass({
   render: function() {
     var vm = this.props.data;
     return (
-      <tr>
-        <th className="th-vm-status"><i className="fa fa-circle"> </i></th>
-        <td>{vm.hostname}</td>
-        <td>{vm.ip}</td>
-        <td>{vm.domain}</td>
-      </tr>
+      <div className="div-vm-host">
+        <span className="span-vm-hostname">{vm.hostname}</span>
+        <div className="div-vm-secondary">
+          <span className="span-vm-ip">IP: {vm.ip}</span>
+          &nbsp;&nbsp;
+          <span className="span-vm-domain">Domain: {vm.domain}</span>
+        </div>
+      </div>
     );
   }
 });

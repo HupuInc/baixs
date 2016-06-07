8bc111c1e80af9e924a602ca6dacf1ecbbb79659
daijun <daijun@hupu.com>
2016-06-07 17:50:56 +0800

    Add local filter and sort for vmm display

--- ui/vm.jsx
+++ ui/vm.jsx
@@ -1,5 +1,229 @@
 var $ = require('jquery');
 var React = require('react');
+var vmmFilter = {
+  mem: 'mem-all',
+  vmNum: 'vmNum-all'
+};
+var sortType = "sortNumAsc";
+
+function sort() {
+  var visibleSortedItem = $('.div-vm-host-out:visible').toArray();
+  visibleSortedItem.sort(function(a, b) {
+    var memA = parseInt($(a).attr('data-mem'));
+    var numA = parseInt($(a).attr('data-length'));
+    var memB = parseInt($(b).attr('data-mem'));
+    var numB = parseInt($(b).attr('data-length'));
+    switch(sortType) {
+      case 'sortNumAsc':
+        if (numA < numB) {
+          return -1;
+        }
+        else {
+          return 1;
+        }
+        break;
+      case 'sortNumDesc':
+        if (numA < numB) {
+          return 1;
+        }
+        else {
+          return -1;
+        }
+        break;
+      case 'sortMemAsc':
+        if (memA < memB) {
+          return -1;
+        }
+        else {
+          return 1;
+        }
+        break;
+      case 'sortMemDesc':
+        if (memA < memB) {
+          return 1;
+        }
+        else {
+          return -1;
+        }
+        break;
+    }
+  });
+  var hiddenItem = $('.div-vm-host-out:hidden').toArray();
+  var items = visibleSortedItem.concat(hiddenItem);
+  $('#HostList').html(items);
+}
+
+function filter() {
+  var filterClass = "";
+  if (vmmFilter.mem !== 'mem-all') {
+    filterClass += "." + vmmFilter.mem;
+  }
+  if (vmmFilter.vmNum !== 'vmNum-all') {
+    filterClass += "." + vmmFilter.vmNum;
+  }
+
+  if ("" !== filterClass) {
+    $('.div-vm-host-out').hide();
+    $(filterClass).show();
+  }
+  else {
+    $('.div-vm-host-out').show();
+  }
+}
+
+var DropdownItem = React.createClass({
+  handleClick: function(ev) {
+    this.props.handleClick(ev, this.props.item);
+  },
+  render: function() {
+    var item = this.props.item;
+    return (
+      <li><a href="javascript:void(0)" id={item.id} onClick={this.handleClick}>{item.desc}</a></li>
+    );
+  }
+});
+
+var VmmFilter = React.createClass({
+  handleSort: function(e, item) {
+    sortType = item.id;
+    $('#' + item.parentId).html('排序: ' + item.desc);
+    sort();
+    filter();
+  },
+  handleSelect: function(e, item) {
+    var id = item.id;
+    vmmFilter[id.split('-')[0]] = id;
+    var startText = id.split('-')[0] === 'mem' ? '内存检索' : '虚拟机数量检索';
+    $('#' + item.parentId).html(startText + ': ' + item.desc);
+    filter();
+    sort();
+  },
+  render: function() {
+    var sortItem = [{
+      parentId: 'dropdownSortText',
+      id: 'sortNumAsc',
+      desc: '虚拟机数量从低到高',
+      handleClick: this.handleSort
+    },
+    {
+      parentId: 'dropdownSortText',
+      id: 'sortNumDesc',
+      desc: '虚拟机数量从高到低',
+      handleClick: this.handleSort
+    },
+    {
+      parentId: 'dropdownSortText',
+      id: 'sortMemAsc',
+      desc: '内存从低到高',
+      handleClick: this.handleSort
+    },
+    {
+      parentId: 'dropdownSortText',
+      id: 'sortMemDesc',
+      desc: '内存从高到低',
+      handleClick: this.handleSort
+    }];
+
+    var memItem = [{
+      parentId: 'dropdownMemText',
+      id: 'mem-all',
+      desc: '全部',
+      handleClick: this.handleSelect
+    },{
+      parentId: 'dropdownMemText',
+      id: 'mem-32',
+      desc: '32 GB',
+      handleClick: this.handleSelect
+    },
+    {
+      parentId: 'dropdownMemText',
+      id: 'mem-64',
+      desc: '64 GB',
+      handleClick: this.handleSelect
+    },
+    {
+      parentId: 'dropdownMemText',
+      id: 'mem-128',
+      desc: '128 GB',
+      handleClick: this.handleSelect
+    }];
+    var vmNumItem = [];
+    vmNumItem.push({
+      parentId: 'dropdownVmNumText',
+        id: 'vmNum-all',
+        desc: '全部',
+        handleClick: this.handleSelect
+    });
+    for (var i = 0; i < 9; i++) {
+      var desc = i + '';
+      if (i === 8) {
+        desc = i + '以上';
+      }
+      vmNumItem.push({
+        parentId: 'dropdownVmNumText',
+        id: 'vmNum-' + i,
+        desc: desc,
+        handleClick: this.handleSelect
+      })
+    }
+
+    var sortItemList = sortItem.map(function(item){
+      return (
+        <DropdownItem item={item} handleClick={item.handleClick}/>
+      )
+    });
+
+    var memItemList = memItem.map(function(item){
+      return (
+        <DropdownItem item={item} handleClick={item.handleClick}/>
+      )
+    });
+
+    var vmNumItemList = vmNumItem.map(function(item){
+      return (
+        <DropdownItem item={item} handleClick={item.handleClick}/>
+      )
+    });
+
+    return (
+      <div className="div-filter btn-toolbar" role="toolbar">
+        <div className="btn-group" role="group">
+          <div className="btn-group" role="group">
+            <button className="btn btn-default dropdown-toggle" type="button" id="dropdownSort" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              <span id="dropdownSortText">排序</span>
+              <span className="caret" style={{'margin-left': '7px'}}></span>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownSort">
+              {sortItemList}
+            </ul>
+          </div>
+
+
+          <div className="btn-group div-dropdown" role="group">
+            <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMem" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              <span id="dropdownMemText">内存检索</span>
+              <span className="caret" style={{'margin-left': '7px'}}></span>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMem">
+              {memItemList}
+            </ul>
+          </div>
+
+
+          <div className="btn-group div-dropdown" role="group">
+            <button className="btn btn-default dropdown-toggle" type="button" id="dropdownVmNum" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              <span id="dropdownVmNumText">虚拟机数量检索</span>
+              <span className="caret" style={{'margin-left': '7px'}}></span>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownVmNum">
+              {vmNumItemList}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 
 var HostList = React.createClass({
   request: function(location) {
@@ -30,7 +254,7 @@ var HostList = React.createClass({
         }, 0);
         $('#SpanSearch').html('搜索结果：命中' + count + '条');
         $('.div-vm-hosts-list').hide();
-        $('.div-radio').hide();
+        $('.div-filter').hide();
         $('#SearchResult').show();
         $('.div-vm-hosts-top').trigger('click');
         $('#divSearchIcon').trigger('click');
@@ -43,6 +267,7 @@ var HostList = React.createClass({
   handleBack: function() {
     $('.div-vm-hosts-list').hide();
     $('#SearchResult').hide();
+    $('.div-filter').show();
     this.request();
   },
   getInitialState: function() {
@@ -61,7 +286,7 @@ var HostList = React.createClass({
     });
     return (
       <div>
-
+        <VmmFilter />
         <div id="SearchResult" className="search-result">
           <span id="SpanSearch"></span>
           &nbsp;&nbsp;&nbsp;&nbsp;
@@ -121,9 +346,25 @@ var HostItem = React.createClass({
     var trafficIn = item.metrics['net.if.in[eth0]'] / 1000 / 1000;
     var trafficOut = item.metrics['net.if.out[eth0]'] / 1000 / 1000;
     var trafficProgressLength = trafficIn / (trafficIn + trafficOut) * 100
+    var hostMem = item.metrics['vm.memory.size[total]'] / 1024 / 1024 / 1024;
+    var divHostItemClass = "div-vm-host-out col-md-6 col-xs-12";
+    if (hostMem <= 32) {
+      divHostItemClass += " mem-32 ";
+      hostMem = '32';
+    }
+    else if (hostMem > 32 && hostMem <= 64) {
+      divHostItemClass += " mem-64 ";
+      hostMem = '64';
+    }
+    else if (hostMem > 64 && hostMem <= 128) {
+      divHostItemClass += " mem-128 ";
+      hostMem = '128';
+    }
+
+    divHostItemClass += "vmNum-" + length;
 
     return (
-      <div className="div-vm-host-out col-md-6 col-xs-12">
+      <div className={divHostItemClass} data-mem={hostMem} data-length={length}>
         <div className="div-vm-host-item" style={vmmItemStyle}>
           <div className="div-vmm-title">
             <div className="div-vmm-title-left">
@@ -134,7 +375,7 @@ var HostItem = React.createClass({
               <span className="span-badge badge">{length}</span>
             </div>
             <div className="div-vmm-title-right">
-              <span className="span-vmm-c-m">{item.metrics['system.cpu.num']} C / {(item.metrics['vm.memory.size[total]'] / 1024 / 1024 / 1024).toFixed(2)} G</span>
+              <span className="span-vmm-c-m">{item.metrics['system.cpu.num']} C / {parseFloat(hostMem).toFixed(2)} G</span>
             </div>
           </div>
           <div className="div-vmm-status">
